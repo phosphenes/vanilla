ec055906d701f13820cddda3bcc5ef9cae77873e
Mark Kanning <markaronkanning@gmail.com>
2016-11-03 10:43:48 -0700

    created basics of women.php template

--- src/wp-content/themes/uc/women.php
+++ src/wp-content/themes/uc/women.php
@@ -6,6 +6,29 @@
 get_header();
 the_post();
 ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<title>Inspiring Women</title>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+
+<?php wp_head(); ?>
+
+<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
+<!-- JQUERY -->
+<script src="//code.jquery.com/jquery-2.2.0.min.js"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+
+<!-- SITE STYLES/SCRIPTS -->
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/_uc/styles.css" />
+<script src="<?php echo get_template_directory_uri(); ?>/_uc/script.js"></script>
+
+</head>
+
+<body id="top">
 
 
 
@@ -45,6 +68,8 @@ the_post();
 
 
 
+</body>
+</html>
 
 
 

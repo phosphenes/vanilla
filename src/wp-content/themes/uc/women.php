fae0c573a857f0db4e2a6e26102ccf741e47eab6
Mark Kanning <markaronkanning@gmail.com>
2016-11-04 14:14:35 -0700

    more quote stuff

--- src/wp-content/themes/uc/women.php
+++ src/wp-content/themes/uc/women.php
@@ -118,8 +118,10 @@ the_post();
 <section id="storyQuote" class="green-text">
 	<main>
 		<blockquote>
+			<div class="quote">
 			Deep down inside we know where we belong, and we know that we have worth, and you need to just keep listening to that voice, because there is a path for you.
 			<p>— melissa, wwin grantee</p>
+			</div>
 		</blockquote>
 	</main>
 </section>

79b76ec58a38578077cc247c49c52213f54c5520
scotty <scott@unitedcreations.com>
2016-11-07 15:24:27 -0800

    highestZ video player

--- src/modules/π-vimeoPlayer/π-vimeoPlayer.js
+++ src/modules/π-vimeoPlayer/π-vimeoPlayer.js
@@ -51,7 +51,7 @@ var allPlayers = {}
 
 			π.listen(listenForEsc, 'keydown');
 
-			el.css({display: "block"});
+			el.css({display: "block", zIndex: π.highestZ()});
 
 			setTimeout(function () {
 				el.addClass("on");

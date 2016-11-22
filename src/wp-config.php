109875e3fef14559577ec3397c1c960d6da9421e
scotty <scott@unitedcreations.com>
2016-11-22 15:23:28 -0800

    update db

--- src/wp-config.php
+++ src/wp-config.php
@@ -27,7 +27,7 @@ define('FS_METHOD', 'direct');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'vanilla');
+define('DB_NAME', 'xin');
 
 /** MySQL database username */
 define('DB_USER', 'root');

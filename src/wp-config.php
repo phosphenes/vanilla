7fd5a55d2ad58e8c520bc587099277a37f0a924a
scotty <scott@unitedcreations.com>
2016-11-30 10:32:24 -0800

    fix encoding

--- src/wp-config.php
+++ src/wp-config.php
@@ -41,7 +41,9 @@ define('DB_HOST', '127.0.0.1');
 //define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+//define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'latin1');
+
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
